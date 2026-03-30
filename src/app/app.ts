67506d7741ed70dcdf4c6c68a1740e496f2fefc0
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2026-03-30 19:35:56 +0530

    connect data angular laravel each other

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Api } from './services/api';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular_test');
+
+   constructor(private api: Api) {}
+data: any;
+  ngOnInit() {
+    this.api.getTest().subscribe({
+      next: (res) => {
+        console.log('API Response:', res);
+        this.data = res;
+      },
+      error: (err) => {
+        console.error('API Error:', err);
+      }
+    });
+  }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://backend-api-i6of.onrender.com/api' // your deployed backend
+};
